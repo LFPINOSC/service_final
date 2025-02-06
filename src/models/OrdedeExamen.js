626e37b2a26b4f5fce1dbc541d9079410f8e9893
LFPINOSC <66181560+LFPINOSC@users.noreply.github.com>
2025-02-06 08:54:23 -0500

    guaradar

--- src/models/OrdedeExamen.js
+++ src/models/OrdedeExamen.js
@@ -6,44 +6,44 @@ import Medico from './Medico.js';
 import Atencion from './Atencion.js';
 
 const OrdendeExamen = sequelize.define('OrdendeExamen', {
-secuencial: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    autoIncrementIdentity: true,
-},
-secuencialAtencion: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references:{
-        model:Atencion,
-        key:'secuencial'
-    }
-},
-secuencialExamen: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references:{
-        model:Examen,
-        key:'secuencial'
-    }
-},
-cedulaPersona:{
-    type: DataTypes.SRING(10),
-    allowNull: false,
-    references:{
-        model:Persona,
-        key:'cedula'
-    }
-},
-cedulaMedico:{
-    type: DataTypes.STRING(10),
-    allowNull: false,
-    references:{
-        model:Medico,
-        key:'cedula'
-    }
-},
+    secuencial: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        autoIncrementIdentity: true,
+    },
+    secuencialAtencion: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model:Atencion,
+            key:'secuencial'
+        }
+    },
+    secuencialExamen: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model:Examen,
+            key:'secuencial'
+        }
+    },
+    cedulaPersona:{
+        type: DataTypes.SRING(10),
+        allowNull: false,
+        references:{
+            model:Persona,
+            key:'cedula'
+        }
+    },
+    cedulaMedico:{
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        references:{
+            model:Medico,
+            key:'cedula'
+        }
+    },
 });
 
 
--- src/models/ResultadoExamen.js
+++ src/models/ResultadoExamen.js
@@ -3,40 +3,40 @@ import sequelize from '../config/sequelizeConfig.js';
 import OrdendeExamen from './OrdedeExamen.js';
 
 const ResultadoExamen = sequelize.define('ResultadoExamen', {
-secuencial: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    autoIncrementIdentity: true,
-},
+    secuencial: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        autoIncrementIdentity: true,
+    },
 
-resultado: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
-    field: 'resultado',
-},
-fecha: {
-    type: DataTypes.DATA,
-    allowNull: false,
-},
-observaciones: {
-    type: DataTypes.STRING(40),
-    allowNull: false,
-    field: 'observaciones',
-},
-estaActivo: {
-    type: DataTypes.SMALLINT,
-    allowNull: false,
-    defaultValue: 1,
-},
-secuencialOrdedeExamen: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references:{
-        model:OrdendeExamen,
-        key:'secuencial'
-    }
-},
+    resultado: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        field: 'resultado',
+    },
+    fecha: {
+        type: DataTypes.DATA,
+        allowNull: false,
+    },
+    observaciones: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+        field: 'observaciones',
+    },
+    estaActivo: {
+        type: DataTypes.SMALLINT,
+        allowNull: false,
+        defaultValue: 1,
+    },
+    secuencialOrdedeExamen: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model:OrdendeExamen,
+            key:'secuencial'
+        }
+    },
 });
 
 export default ResultadoExamen;
